6d86f8718179c83c1215d367e86ce149ed657074
Thomas Dau <ThomasQDau@gmail.com>
2019-04-03 13:10:49 -0700

    made Kairos do a face detailing

--- pages/gym-authentication-page/app.js
+++ pages/gym-authentication-page/app.js
@@ -17,7 +17,11 @@ $('.next1').on('click', function () {
     var picture = null;
     database.ref().once('value', function (data) {
         picture = data.child(idNumber).child('picture').val();
-        $('#youimg').append('<img src="' + picture + '">')
+        if (picture != null) {
+            $('#youimg').append('<img src="' + picture + '">')
+        } else {
+            $('#youimg').html('Your credentials are incorrect!');
+        }
     });
     $('#id_number').empty();
     $('.step2').show();
@@ -32,10 +36,11 @@ function enterPressed(e) {
     }
 }
 
-// database.ref().once('value', function(data) {
-//     console.log(data.child('5837836254').child('picture').val());
-// });
+database.ref().once('value', function (data) {
+    console.log(data.child('4320552453').child('picture').val());
+});
 
+//Sends image to Kairos and returns a console log with details of the face recognition
 // var headers = {
 //     "Content-type": "application/json",
 //     "app_id": "b9f2d60a",
@@ -43,14 +48,17 @@ function enterPressed(e) {
 // };
 // var picture = null;
 // var payload = { "image": "" };
-// payload = { "image": picture };
-// var url = "http://api.kairos.com/detect";
-// // make request 
-// $.ajax(url, {
-//     headers: headers,
-//     type: "POST",
-//     data: JSON.stringify(payload),
-//     dataType: "text"
-// }).done(function (response) {
-//     console.log(response);
+// database.ref().once('value', function (data) {
+//     picture = data.child('4320552453').child('picture').val();
+//     payload = { "image": picture };
+//     var url = "http://api.kairos.com/detect";
+//     // make request 
+//     $.ajax(url, {
+//         headers: headers,
+//         type: "POST",
+//         data: JSON.stringify(payload),
+//         dataType: "text"
+//     }).done(function (response) {
+//         console.log(response);
+//     });
 // });
